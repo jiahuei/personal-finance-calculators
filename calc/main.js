3ea8afc11dffe856c7b81ccd1eb471d62afc8059
Tan Jia Huei <tanjiahuei@gmail.com>
2021-12-22 00:12:33 +0800

    add photo credits page

--- calc/main.js
+++ calc/main.js
@@ -227,7 +227,8 @@ function addFooter() {
 <div class="container has-text-centered">
   <p>
     Made by <a href="https://github.com/jiahuei">Jia-Huei Tan</a>. The source code and website content are
-    licensed <a href="https://choosealicense.com/licenses/gpl-3.0/">GPL-3.0</a>.
+    licensed <a href="https://choosealicense.com/licenses/gpl-3.0/">GPL-3.0</a>. 
+    <a href="photo-credits.html">Click here for photo credits</a>.
   </p>
   <p>
     Complete source code of licensed works and modifications, which include larger works using a licensed work, 
